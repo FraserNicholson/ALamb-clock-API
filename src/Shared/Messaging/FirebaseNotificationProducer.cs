10fb7675edbb072779249b50b4239a7683f640c6
Fraser Nicholson <fraser.nicholson@btinternet.com>
2023-08-06 11:31:22 +0100

    Send firebase notifications instead of a data notification

--- src/Shared/Messaging/FirebaseNotificationProducer.cs
+++ src/Shared/Messaging/FirebaseNotificationProducer.cs
@@ -1,4 +1,5 @@
 ﻿using FirebaseAdmin.Messaging;
+using Shared.Models;
 using Shared.Models.Database;
 
 namespace Shared.Messaging;
@@ -13,16 +14,30 @@ public class FirebaseNotificationProducer : INotificationProducer
     {
         foreach (var notification in notifications)
         {
-            var message = new MulticastMessage()
+            var message = new MulticastMessage
             {
                 Tokens = notification.RegistrationTokens,
-                Data = new Dictionary<string, string>()
+                Notification = new Notification
                 {
-                    { "NotificationId", notification.Id },
+                    Title = GetNotificationTitle(notification),
+                    Body = GetNotificationBody(notification)
                 }
             };
 
             await FirebaseMessaging.DefaultInstance.SendMulticastAsync(message);
         }
+    }
+
+    private static string GetNotificationTitle(NotificationDbModel notification)
+        => $"{notification.Team1} vs {notification.Team2}";
+
+    private static string GetNotificationBody(NotificationDbModel notification)
+    {
+        return notification.NotificationType switch
+        {
+            NotificationType.InningsStarted => $"{notification.TeamInQuestion} have started batting",
+            NotificationType.WicketCount => $"{notification.TeamInQuestion} are {notification.NumberOfWickets} down",
+            _ => string.Empty
+        };
     }
 }
